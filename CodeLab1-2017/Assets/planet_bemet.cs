14e4f0fb43776dc53ed07542e61d6634790176f1
lefton22 <lefton22@aliyun.com>
2017-02-01 15:09:39 -0500

    c

---
+++ CodeLab1-2017/Assets/planet_bemet.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class planet_bemet : MonoBehaviour {
+	
+	public float life;
+	public float plusSpeed;
+    string name_Meet;
+	GameObject meet;
+	float scale_meet;
+
+    float minimum ;
+    float maximum ;
+	float t = 0.0f;
+	bool isMeet;
+
+
+
+
+	void Start () {
+		//Debug.Log ("radius: " + GetComponent<CircleCollider2D>().radius );
+		isMeet = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		life = life - plusSpeed;
+		//Debug.Log ("life: " + life);
+
+
+
+		transform.localScale = new Vector3 (life,life,1.0f);
+		GetComponent<CircleCollider2D> ().radius = transform.localScale.x/15;
+
+		if (isMeet )
+		{
+         	t += 0.5f * Time.deltaTime;
+//			minimum = transform.localScale.x;
+//			maximum = meet.transform.localScale.x + transform.localScale.x;
+
+			transform.localScale = new Vector3(Mathf.Lerp(minimum, maximum, t), Mathf.Lerp(minimum, maximum, t), 1);
+
+			//meet.SendMessage ("destroy");
+
+			//life = maximum;
+
+			if (transform.localScale.x == maximum ) 
+			{
+				
+				isMeet = false;
+				Debug.Log ("false!!!");
+			}
+
+		}
+
+		if (life <= 0) 
+		{
+			Debug.Log ("GAME OVER");
+			SceneManager.LoadScene ("gameover");
+		}
+	}
+
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		Debug.Log ("collide !");
+		name_Meet = collider.name;
+		Debug.Log ("name_Meet : " +name_Meet);
+
+		meet = collider.gameObject;   //碰撞到的“那个物体”管他叫什么，可加一个附加判断条件，if+ TAG...
+		Debug.Log ("GameObject meet : " + meet);
+
+		scale_meet = meet.transform.localScale.x;
+		Debug.Log ("scale_meet : " + scale_meet);
+
+
+		minimum = transform.localScale.x;
+		maximum = meet.transform.localScale.x + transform.localScale.x;
+
+		life = maximum;
+
+		//			life = life + 0.05f;
+
+		Destroy (meet);
+
+		toSclaeMeet ();
+
+
+
+
+	}
+
+	void toSclaeMeet ()
+	{
+
+		isMeet = true;
+		Debug.Log ("become bigger and bigger until ...isMeet: " + isMeet);
+
+	}
+}
---
+++ CodeLab1-2017/Assets/player_control.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_control : MonoBehaviour {
+
+	public float speed_followCurse =0.001f;
+	float timeDeltaTime;
+
+	float speed_myPlanet;
+
+	void Start () 
+	{
+		speed_myPlanet = 0.07f;
+	}
+
+	void Update () 
+	{
+		//speed_myPlanet = speed_myPlanet + 0.05f;
+////////move
+
+		Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		pz.z = 0;
+
+		transform.Translate (pz * speed_followCurse);	
+
+		//Debug.Log ("where's mouse: " +pz);
+
+//		if (Input.GetKey(KeyCode.Mouse0)) 
+//		{
+//			//Debug.Log ("clicked mouse0");
+//			//transform.position = Vector3.MoveTowards(transform.position, pz, speed);
+//			//a = a+ pz.x; 
+//			//transform.LookAt(pz);
+//			transform.Translate (Vector3.up);	
+//		}
+
+
+////////rotate
+		//transform.rotation = new Vector3( 0, 0, speed_myPlanet);
+		//transform.Rotate( 0 ,0, speed_myPlanet);
+	}
+		
+}
